e36eed43a9170eca5a5bacfe812aafaf7b27fbf1
kyuuki <kyuuki.japan@gmail.com>
2021-12-31 14:19:29 +0000

    ログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_page#top'
   # root to: 'static_page#top'  # 上記はこれの省略形
+
+  #
+  # ログイン
+  #
+  get 'users/log_in', to: 'sessions#new'
+  post 'users/log_in', to: 'sessions#create'
+  get 'users/log_out', to: 'sessions#delete'
 end
